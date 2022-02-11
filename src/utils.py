bd7474c101dca4c136957453b3f0e1298b8c23f9
Andrew Schaub <andrew.schaub@protonmail.com>
2022-02-11 12:51:21 -0600

    added data extraction to utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
+import gzip
 import os
+import tarfile
 import urllib
 from pathlib import Path
 
-def fetchData(dataFile, dataURL, dataPath):
+def fetchData(dataFile, dataURL, dataPath, extract=False):
     """fetches remote datafiles from URLs and saves in local datapath
     
     Path works for local path manipulation to ensure compatiability across
@@ -13,9 +15,31 @@ def fetchData(dataFile, dataURL, dataPath):
         dataFile (string): filename
         dataURL (string): root data URL
         dataPath (Path): local data path
+        extract (Boolean): option to extract tarfiles
+        extractType (string): specify compression type: ['gunzip', 'tar']
 
     """
     os.makedirs(dataPath, exist_ok=True)
     dataFilepath = Path(dataPath, dataFile)
     fulldataURL = dataURL + dataFile
     urllib.request.urlretrieve(fulldataURL, dataFilepath)
+
+    if extract:
+        compressedFile  = dataFilepath
+        while compressedFile.suffix in {'.tar', '.gz', '.zip'}:
+            decompressedFile = compressedFile.with_suffix('')
+
+        if compressedFile.suffix == 'tgz':
+            tgzFile = tarfile.open(compressedFile)
+            tgzFile.extractall(path=dataPath)
+            tgzFile.close()
+
+        if compressedFile.suffix == 'gz':
+            blockSize=65536
+
+            input = gzip.GzipFile(compressedFile, 'wb').read()
+            input.close()
+
+            output = open(decompressedFile, 'wb')
+            output.write(input)
+            output.close()
