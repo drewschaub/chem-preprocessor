7ceaaab3b754ab2d1dd8d022c73c4fbee098627c
Andrew Schaub <andrew.schaub@protonmail.com>
2022-02-11 12:22:45 -0600

    pulled functions from notebook for future use

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import os
+import urllib
+from pathlib import Path
+
+def fetchData(dataFile, dataURL, dataPath):
+    """fetches remote datafiles from URLs and saves in local datapath
+    
+    Path works for local path manipulation to ensure compatiability across
+    operating sytems, but does not work for URLs. urllib is used for URLs
+    and so uses strings, as opposed to Path objects.
+
+    Args:
+        dataFile (string): filename
+        dataURL (string): root data URL
+        dataPath (Path): local data path
+
+    """
+    os.makedirs(dataPath, exist_ok=True)
+    dataFilepath = Path(dataPath, dataFile)
+    fulldataURL = dataURL + dataFile
+    urllib.request.urlretrieve(fulldataURL, dataFilepath)
